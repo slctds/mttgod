dd334baaceadb197b43d17811961ba286788accd
slctds <pavel.selected@gmail.com>
2024-06-29 11:30:19 -0300

    local version for development

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -6,34 +7,39 @@ const http = require('http');
 const https = require('https');
 
 const app = express();
-const port = 3000;
-const sslPort = 3443;
-
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/pokertest.online/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/pokertest.online/fullchain.pem')
-};
+const port = process.env.PORT || 3000;
+const sslPort = process.env.SSL_PORT || 3443;
+const useSsl = process.env.USE_SSL === 'true';
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Обработчик для корневого URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/api/images', (req, res) => {
     const imagesDirectory = path.join(__dirname, 'public/50bb');
-    console.log(`Reading directory: ${imagesDirectory}`); // Добавим логирование пути к директории
-
     fs.readdir(imagesDirectory, (err, files) => {
         if (err) {
-            console.error(`Failed to read directory: ${err.message}`); // Добавим логирование ошибки
             return res.status(500).json({ error: 'Failed to read directory' });
         }
         res.json(files);
     });
 });
 
-http.createServer(app).listen(port, () => {
-    console.log(`HTTP Server running at http://localhost:${port}`);
-});
+if (useSsl) {
+    const options = {
+        key: fs.readFileSync(process.env.SSL_KEY_PATH || 'path/to/your/ssl/key.pem'),
+        cert: fs.readFileSync(process.env.SSL_CERT_PATH || 'path/to/your/ssl/cert.pem')
+    };
 
-https.createServer(options, app).listen(sslPort, () => {
-    console.log(`HTTPS Server running at https://localhost:${sslPort}`);
-});
+    https.createServer(options, app).listen(sslPort, () => {
+        console.log(`HTTPS Server running at https://localhost:${sslPort}`);
+    });
+} else {
+    http.createServer(app).listen(port, () => {
+        console.log(`HTTP Server running at http://localhost:${port}`);
+    });
+}
